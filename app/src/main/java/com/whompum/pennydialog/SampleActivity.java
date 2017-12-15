af73e5c9abfd2973fde37abfa555de88723a6db5
bryan <bryanmills86@yahoo.com>
2017-12-15 02:22:02 -0800

    Added sample

--- app/src/main/java/com/whompum/pennydialog/SampleActivity.java
+++ app/src/main/java/com/whompum/pennydialog/SampleActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.whompum.pennydialog.dialog.PennyDialog;
 
@@ -20,7 +21,17 @@ public class SampleActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               PennyDialog dialog = PennyDialog.newInstance(null, null);
+               PennyDialog dialog = PennyDialog.newInstance(new PennyDialog.CashChangeListener() {
+                   @Override
+                   public void onPenniesChange(long pennies) {
+                        Toast.makeText(SampleActivity.this, String.valueOf(pennies), Toast.LENGTH_SHORT).show();
+                   }
+
+                   @Override
+                   public void onCashChange(String cashValue) {
+                       //Toast.makeText(SampleActivity.this, cashValue, Toast.LENGTH_SHORT).show();
+                   }
+               }, null);
                dialog.show(getSupportFragmentManager(), PennyDialog.TAG);
             }
         });
