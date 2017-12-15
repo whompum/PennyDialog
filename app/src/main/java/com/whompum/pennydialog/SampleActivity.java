ebd9601be600ab5520a037b059ffb405bdf6977c
bryan <bryanmills86@yahoo.com>
2017-12-15 00:52:54 -0800

    Update sample module resources

---
+++ app/src/main/java/com/whompum/pennydialog/SampleActivity.java
@@ -0,0 +1,29 @@
+package com.whompum.pennydialog;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.whompum.pennydialog.dialog.PennyDialog;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               PennyDialog dialog = PennyDialog.newInstance(null, null);
+               dialog.show(getSupportFragmentManager(), PennyDialog.TAG);
+            }
+        });
+    }
+
+}
