4477ade0f350ba4e84129722afd122a3a5cdeea8
bryan <bryanmills86@yahoo.com>
2017-12-25 22:41:24 -0800

    Add shadow logic

---
+++ dialog/src/main/java/com/whompum/pennydialog/dialog/ShadowLayout.java
@@ -0,0 +1,51 @@
+
+package com.whompum.pennydialog.dialog;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by bryan on 12/25/2017.
+ */
+
+public class ShadowLayout extends FrameLayout {
+
+
+    public ShadowFrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public ShadowFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShadowFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public ShadowLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        final Drawable background = new ShadowDrawable(new RectShape(), getResources());
+        background.setBounds(0,0, w, h);
+
+        if(Build.VERSION.SDK_INT >= 16)
+            setBackground(background);
+        else
+            setBackgroundDrawable(background);
+    }
+}
