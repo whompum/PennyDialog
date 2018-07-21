20c243ffe1c84ba35f11966838db7d62d84d6a61
bryan <bryanmills86@yahoo.com>
2018-07-21 11:18:21 -0700

    Accomodate and standardize sizing for different screen sizes

--- app/src/main/java/com/whompum/pennydialog/SampleActivity.java
+++ app/src/main/java/com/whompum/pennydialog/SampleActivity.java
@@ -1,10 +1,13 @@
 package com.whompum.pennydialog;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -40,6 +43,12 @@ public class SampleActivity extends AppCompatActivity {
 
             }
         });
+
+
+        Configuration c = getResources().getConfiguration();
+
+        Log.i("LAYOUT_FIX", "SCREEN WIDTH dp: " + c.screenWidthDp);
+
 
     }
 
